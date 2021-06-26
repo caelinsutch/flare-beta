962e89507235e9800b52635836a2f08d5be7e1a0
Caelin Sutch <caelinsutch@gmail.com>
2021-06-25 18:52:14 -0700

    Fix build problems

--- src/Api/Handlers/register.ts
+++ src/Api/Handlers/register.ts
@@ -2,7 +2,7 @@ import { userCollection } from "../Firebase/firestore";
 import { User } from "../../Models/User";
 import { sendText } from "../Twilio";
 
-const register = async (user: Omit<User, "createdAt">) => {
+const register = async (user: Omit<User, "createdAt" | "userId">) => {
   const u = await userCollection.where("phone", "==", user.phone).get();
 
   if (u.size > 0) {
