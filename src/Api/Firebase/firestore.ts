36d079c65fa61171c7a5c3c7901f6a3762d6553a
Caelin Sutch <caelinsutch@gmail.com>
2021-07-23 11:48:30 -0400

    Update user collection

--- src/Api/Firebase/firestore.ts
+++ src/Api/Firebase/firestore.ts
@@ -1,6 +1,6 @@
 import { firestore } from "./admin";
 
-const userCollection = firestore.collection("users");
+const userCollection = firestore.collection("users-v2");
 const partyCollection = firestore.collection("parties");
 
 export { userCollection };
