75c0e504749a847a05314d7787d98ea41c351a81
Caelin Sutch <caelinsutch@gmail.com>
2021-07-23 11:49:14 -0400

    Redeploy

--- src/Api/Firebase/firestore.ts
+++ src/Api/Firebase/firestore.ts
@@ -1,6 +1,5 @@
 import { firestore } from "./admin";
 
 const userCollection = firestore.collection("users-v2");
-const partyCollection = firestore.collection("parties");
 
 export { userCollection };
